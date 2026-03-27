02967d8307cc9914d29ffbd5f93c6c92b575ff80
Andrei <andfishka@gmail.com>
2026-03-27 21:26:29 +0300

    feat:implement the folding of shapes

--- MyTetris.js
+++ MyTetris.js
@@ -1,4 +1,8 @@
 class TetrisMode extends BaseMode {
+	static COLLISION_OK = 0;
+	static COLLISION_BLOCKED = 1;
+	static COLLISION_X = 2;
+
 	#TETROMINOES = {
 		I: [[1, 1, 1, 1]],
 		O: [
@@ -28,12 +32,14 @@ class TetrisMode extends BaseMode {
 	};
 
 	#bitmap = Array(16).fill(0x0);
+	#bitmask = Array(16).fill(0x0);
+
 
 
 	constructor(ctx) {
 		super(ctx);
 
-		this.pos= {};
+		this.pos = {};
 		this.offset = {}
 		this.currentTetro = this.createFig();
 
@@ -51,6 +57,10 @@ class TetrisMode extends BaseMode {
 		setInterval(() => {
 			this.moveTetromino(0, 1);
 		}, 500);
+
+		this.moveTetromino(0, 1);
+
+
 	}
 
 	exit(nextMode) {
@@ -125,18 +135,60 @@ class TetrisMode extends BaseMode {
 		this.matrixDraw(this.currentTetro, this.pos.x, this.pos.y)
 	}
 
+	removeFullLines() {
+		for (let row = this.#bitmask.length - 1; row >= 0; row--) {
+			if (this.#bitmask[row] === 0xFF) {
+				// сдвигаем все строки выше вниз на 1
+				for (let y = row; y > 0; y--) {
+					this.#bitmask[y] = this.#bitmask[y - 1];
+				}
+				// верхняя строка пустая
+				this.#bitmask[0] = 0x00;
+
+				// важно: повторно проверяем этот же row,
+				// потому что сюда приехала новая строка сверху
+				row++;
+			}
+		}
+	}
 
 	moveTetromino(x, y) {
-		if (this.pos.x + x >= 0 && this.pos.x + x + this.offset.x < 8) {
-			this.pos.x += x;
+
+		const move = {
+			x: this.pos.x + x,
+			y: this.pos.y + y
 		}
-		if (this.pos.y + this.offset.y > 16) {
-			this.currentTetro = this.createFig();
-		} else {
-			this.pos.y += y;
+
+		const state = this.checkCollision(this.currentTetro, move.x, move.y);
+
+		switch (state) {
+			case TetrisMode.COLLISION_OK:
+				this.pos.x = move.x;
+				this.pos.y = move.y;
+				break;
+
+			case TetrisMode.COLLISION_X: {
+				// const stateY = this.checkCollision(this.currentTetro, this.pos.x, this.pos.y + y);
+				// if (stateY === TetrisMode.COLLISION_OK) {
+				this.pos.y += y;
+				// } 
+				// else if (stateY === TetrisMode.COLLISION_BLOCKED) {
+				// 	this.mergeBitmapToMask();
+				// 	this.currentTetro = this.createFig();
+				// }
+				break;
+			}
+
+			case TetrisMode.COLLISION_BLOCKED:
+				this.mergeBitmapToMask();
+				this.removeFullLines();
+				this.currentTetro = this.createFig();
+				break;
 		}
+
 		this.matrixDraw(this.currentTetro, this.pos.x, this.pos.y, this.currentRotation)
 	}
+
 
 	createFig() {
 		// Получаем массив ключей объекта
@@ -157,43 +209,114 @@ class TetrisMode extends BaseMode {
 		return randomTetromino;
 	}
 
-	setOffset(arr){
-		console.log('n: %d, m: %d.', arr[0].length -1, arr.length -1)
-		return {x: arr[0].length -1, y: arr.length -1};
+	setOffset(arr) {
+		return { x: arr[0].length - 1, y: arr.length - 1 };
 	}
 
 	matrixDraw(figure, x, y) {
-		// Очищаем bitmap перед отрисовкой
-		this.#bitmap.fill(0x0);
+		this.#bitmap.fill(0);
 
 		for (let i = 0; i < figure.length; i++) {
 			for (let j = 0; j < figure[i].length; j++) {
 				if (figure[i][j] === 1) {
-					const bitmapIndex = 8 + x + j;
+					const bitmapIndex = y + i;
 					if (bitmapIndex >= 0 && bitmapIndex < this.#bitmap.length) {
 						// Устанавливаем бит в позиции j
-						this.#bitmap[bitmapIndex] |= (1 << (i + y));
+						this.#bitmap[bitmapIndex] |= (1 << (7 - (x + j)));
 					}
 				}
 			}
 		}
 
+
+
+		const transformArr = Array(16).fill(0x0);
+
+		for (let i = 0; i < transformArr.length; i++) {
+			let tansMask = 0x00;
+			let n = 0;
+			// Берём колонку, но в перевёрнутом порядке
+			if (i < 8) {
+				n = 8;
+			}
+			else {
+				n = 0;
+			}
+			for (let j = 0; j < 8; j++) {
+				let m = n + j;
+				const col = this.#bitmask[m] | this.#bitmap[m];
+				// Проверяем бит j в колонке col
+				const bit = (col >> (7 - i % 8)) & 1;
+
+				if (bit === 1) {
+					tansMask |= (1 << j);
+				}
+			}
+
+			transformArr[i] = tansMask;
+		}
+
+		// this.#bitmap.forEach((num, index) => {
+		// 	const transformNum = transformArr[index];
+		// 	console.log(`${String(index + 1).padStart(2, '0')}: 0b${num.toString(2).padStart(8, '0')} 0b${transformNum.toString(2).padStart(8, '0')}`);
+		// });
+
+
+
 		// Отрисовка на физической матрице
 		for (let matrixNum = 0; matrixNum < 2; matrixNum++) {
 			for (let i = 1; i < 9; i++) {
-				const bitmapIndex = matrixNum * 8 + i - 1;
+				const bitmapIndex = (matrixNum * 8 + i - 1);
+				// let mergeBits = this.#bitmap[bitmapIndex]; // | this.#bitmask[bitmapIndex]
+				let mergeBits = transformArr[bitmapIndex];
 
-				this.ctx.matrix.maxWrite(i + (matrixNum * 8), this.#bitmap[bitmapIndex]);
+				this.ctx.matrix.maxWrite(i + (matrixNum * 8), mergeBits);
 
 			}
 		}
-		// this.#bitmap.forEach((num, index) => {
+		// transformArr.forEach((num, index) => {
 		// 	console.log(`Элемент ${index}: 0b${num.toString(2).padStart(8, '0')}`);
 		// });
 
 		this.ctx.matrix.draw();
-
 	}
+
+	mergeBitmapToMask() {
+		for (let i = 0; i < this.#bitmap.length; i++) {
+			this.#bitmask[i] |= this.#bitmap[i];
+		}
+	}
+
+	checkCollision(figure, x, y) {
+		for (let i = 0; i < figure.length; i++) {
+			for (let j = 0; j < figure[i].length; j++) {
+				if (figure[i][j] !== 1) continue;
+
+				const bx = x + j;
+				const by = y + i;
+
+				// стенки по X
+				if (bx < 0 || bx >= 8) {
+					return TetrisMode.COLLISION_X;
+				}
+
+				// низ
+				if (by >= 16) {
+					return TetrisMode.COLLISION_BLOCKED;
+				}
+
+				// столкновение с маской
+				if (by >= 0 && (this.#bitmask[by] & (1 << (7 - bx)))) {
+					// если пробовали смещение по X — считаем X-коллизией
+					if (x !== this.pos.x) return TetrisMode.COLLISION_X;
+					return TetrisMode.COLLISION_BLOCKED;
+				}
+			}
+		}
+		return TetrisMode.COLLISION_OK;
+	}
+
+
 
 	onMinute() {
 		// Обычно пусто для тетриса
